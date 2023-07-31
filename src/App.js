9c4df9b6cba287b7545b7b7538626b82ce811e14
IvanBanifatsyeu <gavrik2010@yahoo.com>
2023-07-31 17:10:29 +0300

    useRequest

--- src/App.js
+++ src/App.js
@@ -5,30 +5,43 @@ import Hover from "./components/Hover";
 import useHover from "./hooks/useHover";
 import List from "./components/List";
 import useDebounce from "./hooks/useDebounce";
-
+import axios from "axios";
+import useRequest from "./hooks/useRequest";
 
 function App() {
 	// const userName = useInput("");
 	// const password = useInput("");
 	// const ref1 = useRef()
 	// const isHovering = useHover(ref1)
-	const [value, setValue] = useState('')
-    const debouncedSearch = useDebounce(search, 500)
+	// const [value, setValue] = useState('')
+	// const debouncedSearch = useDebounce(search, 500)
 
-	function search (query) {
-		fetch(`https://jsonplaceholder.typicode.com/todos?query=`+ query)
-			.then((response) => response.json())
-			.then((json) => {console.log(json)});
-	};
+	// function search (query) {
+	// 	fetch(`https://jsonplaceholder.typicode.com/todos?query=`+ query)
+	// 		.then((response) => response.json())
+	// 		.then((json) => {console.log(json)});
+	// };
 
+	// const onChange = (e)=> {setValue(e.target.value);
+	// debouncedSearch(e.target.value)}
+
+	const [todos, loading, error] = useRequest(fetchTodos);
 	
-	const onChange = (e)=> {setValue(e.target.value);
-	debouncedSearch(e.target.value)}
-	
+	function fetchTodos() {
+		return axios.get(`https://jsonplaceholder.typicode.com/todos`);
+	}
+
+	if (loading) {
+		return <h1>Идет загрузка</h1>
+	}
+
+	if (error) {
+		return <h1>Произошла ошибка при загрузке</h1>
+	}
 
 	return (
 		<div>
-			<input type="text" value={value} onChange={onChange}/>
+			{/* <input type="text" value={value} onChange={onChange}/> */}
 			{/* <Hover />
 			<div ref={ref1} style={{ width: 300, height: 300, background: isHovering ? 'orange' : 'grey' }}></div>
 			<hr />
@@ -44,7 +57,20 @@ function App() {
 			>
 				click
 			</button> */}
-		
+			{todos && todos.map((todo) => {
+				return (
+					<div
+						key={todo.id}
+						style={{
+							border: `2px solid ${todo.id % 2 === 0 ? "red" : "blue"}`,
+							padding: 5,
+							margin: 2,
+						}}
+					>
+						{todo.id}. {todo.title}
+					</div>
+				);
+			})}
 		</div>
 	);
 }
---
+++ src/hooks/useRequest.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export default function useRequest (request){
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {request().then(response => setData(response.data)).catch( error => setError(error)).finally(() => setLoading(false))}, 2000)
+    }, [])
+
+    return [data, loading, error]
+}
